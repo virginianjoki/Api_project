69fcce79ae5baa46e665c024bf71c3facddc9dba
virginianjoki <njokivirginia0619@gmail.com>
2024-12-14 11:11:07 +0300

    rest-framework

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,8 +1,10 @@
 # posts/urls.py
 from django.urls import path
 from .views import PostListCreateView, PostDetailView
+from .views import secure_endpoint
 
 urlpatterns = [
     path('', PostListCreateView.as_view(), name='post_list'),
     path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('api/secure/', secure_endpoint, name='secure-endpoint'),
 ]
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -2,6 +2,15 @@
 from rest_framework import generics, permissions
 from .models import Post
 from .serializers import PostSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def secure_endpoint(request):
+    return Response({"message": "You have access to this endpoint!"})
+
 
 class PostListCreateView(generics.ListCreateAPIView):
     queryset = Post.objects.all()
